8cb854c425952bf2b5ddd40ee1dca83fb1021e2a
ZagZx <zagzcraft@gmail.com>
2023-07-14 08:24:08 -0300

    teste

--- ESTUDAR.py
+++ ESTUDAR.py
@@ -1,29 +1,47 @@
 import random
 def linha():
-    print("  |--------------------|")
+    print("  |-------------------|")
 
 def jogo():
-    print(a)
+    print(a1)
     linha()
-    print(b)
+    print(b1)
     linha()
-    print(c)
+    print(c1)
     linha()
-    print(d)
+    print(d1)
     linha()
-    print(e)
+    print(e1)
+
+def jogolimpo():
+    print(a1.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," "))
+    print(b1.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," "))
+    print(c1.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," "))
+    print(d1.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," "))
+    print(e1.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," "))
 
 
-a = "A |1|2|3|4|5|6|7|8|9|10|"
-b = "B |1|2|3|4|5|6|7|8|9|10|"
-c = "C |1|2|3|4|5|6|7|8|9|10|"
-d = "D |1|2|3|4|5|6|7|8|9|10|"
-e = "E |1|2|3|4|5|6|7|8|9|10|"
+
+a = "A |1|2|3|4|5|6|7|8|9|0|"
+b = "B |1|2|3|4|5|6|7|8|9|0|"
+c = "C |1|2|3|4|5|6|7|8|9|0|"
+d = "D |1|2|3|4|5|6|7|8|9|0|"
+e = "E |1|2|3|4|5|6|7|8|9|0|"
+
+a1 = a
+b1 = b
+c1 = c
+d1 = d
+e1 = e
+
 
 startlinha_estrela = random.randrange(1,6)
-startcoluna_estrela = str(random.randrange(1,11))
+startcoluna_estrela = str(random.randrange(0,10))
 startlinha_personagem = random.randrange(1,6)
-startcoluna_personagem = str(random.randrange(1,11))
+startcoluna_personagem = str(random.randrange(0,10))
+
+while startlinha_estrela == startlinha_personagem:
+        startlinha_estrela = random.randrange(1,6)
 
 print("linha estrela: ",startlinha_estrela)
 print("coluna estrela: ",startcoluna_estrela)
@@ -33,86 +51,61 @@ print("coluna personagem: ",startcoluna_personagem)
 
 #estrela
 if startlinha_estrela == 1:
-    if startcoluna_estrela == "10":
-        a = a.replace(startcoluna_estrela, "* ")
-    else:
-        a = a.replace(startcoluna_estrela, "*")
+        a1 = a.replace(startcoluna_estrela, "*")
 
 
 elif startlinha_estrela == 2:
-    if startcoluna_estrela == "10":
-        b = b.replace(startcoluna_estrela, "* ")
-    else:
-        b = b.replace(startcoluna_estrela, "*")
+        b1 = b.replace(startcoluna_estrela, "*")
 
 
 elif startlinha_estrela == 3:
-    if startcoluna_estrela == "10":
-        c = c.replace(startcoluna_estrela, "* ")
-    else:
-        c = c.replace(startcoluna_estrela, "*")
+        c1 = c.replace(startcoluna_estrela, "*")
 
 
 elif startlinha_estrela == 4:
-    if startcoluna_estrela == "10":
-        d = d.replace(startcoluna_estrela, "* ")
-    else:
-        d = d.replace(startcoluna_estrela, "*")
+        d1 = d.replace(startcoluna_estrela, "*")
 
 
 elif startlinha_estrela == 5:
-    if startcoluna_estrela == "10":
-        e = e.replace(startcoluna_estrela, "* ")
-    else:
-        e = e.replace(startcoluna_estrela, "*")
+        e1 = e.replace(startcoluna_estrela, "*")
 
 #personagem
 if startlinha_personagem == 1:
-    if startcoluna_personagem == "10":
-        a = a.replace(startcoluna_personagem, "£ ")
-    else:
-        a = a.replace(startcoluna_personagem, "£")
+        a1 = a.replace(startcoluna_personagem, "£")
 
 
 elif startlinha_personagem == 2:
-    if startcoluna_personagem == "10":
-        b = b.replace(startcoluna_personagem, "£ ")
-    else:
-        b = b.replace(startcoluna_personagem, "£")
+        b1 = b.replace(startcoluna_personagem, "£")
 
 
 elif startlinha_personagem == 3:
-    if startcoluna_personagem == "10":
-        c = c.replace(startcoluna_personagem, "£ ")
-    else:
-        c = c.replace(startcoluna_personagem, "£")
+        c1 = c.replace(startcoluna_personagem, "£")
 
 
 elif startlinha_personagem == 4:
-    if startcoluna_personagem == "10":
-        d = d.replace(startcoluna_personagem, "£ ")
-    else:
-        d = d.replace(startcoluna_personagem, "£")
+        d1 = d.replace(startcoluna_personagem, "£")
 
 
 elif startlinha_personagem == 5:
-    if startcoluna_personagem == "10":
-        e = e.replace(startcoluna_personagem, "£ ")
-    else:
-        e = e.replace(startcoluna_personagem, "£")
+        e1 = e.replace(startcoluna_personagem, "£")
 #tentar colocar o de cima em for
 
-a = a.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," ")
-b = b.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," ")
-c = c.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," ")
-d = d.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," ")
-e = e.replace("1"," ").replace("2"," ").replace("3"," ").replace("4"," ").replace("5"," ").replace("6"," ").replace("7"," ").replace("8"," ").replace("9"," ").replace("0"," ")
 
 jogo()
+jogolimpo()
 
-#for a in range(0,999999999999):
- #   comando  = input("Digite a direção que o personagem irá de se mover: ")
-    #if comando == "cima":
+for a in range(0,999999999999):
+        comando  = input("Digite a direção que o personagem irá de se mover: ")
+        if comando == "cima" or comando == "Cima":
+                cima = int(input(('Quantos passos para cima? ')))
+        elif comando == 'baixo' or comando == 'Baixo':
+                baixo = int(input(('Quantos passos para baixo? ')))
+        elif comando == 'direita' or comando == 'Direita':
+                direita = int(input(('Quantos passos para direita? ')))
+        elif comando == 'esquerda' or comando == 'Esquerda':
+                esquerda = int(input(('Quantos passos para esquerda? ')))
+        
+
 #inacabado
 
 
